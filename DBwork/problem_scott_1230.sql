9bdea23ab1a88ca29545a7975d0d095d5ea38f38
potato <zpzlzmz@gmail.com>
2024-12-30 17:55:07 +0900

    12-30

---
+++ DBwork/problem_scott_1230.sql
@@ -0,0 +1,62 @@
+-- 12월 30일 oracle 문제 
+
+--1.어떤 종류의 직업이 있는지 job을 한번씩만 출려갛는데 직업의 오룸차순
+select distinct job from emp order by job asc;
+
+--2.ename에 대소문자 상관없이 s를 포함하고 있는데이터 출력
+select ename,job,sal from emp where ename like 'S%';
+
+--3.ename의 3번째 글자가 L인 사람 조회
+select ename,job,sal from emp where ename like '__L%';
+
+--4.comm이 NULL이 아닌사람중에 sal이 1500 이상인 사람들 
+select ename,sal,comm from emp where sal>=1500 and comm is not null;
+
+--5. hiredate 입사일이 5월인 사람은 모두 출력(컬럼:ename,hiredate,sal)?
+select ename,hiredate,sal from emp where to_char(hiredate,'mm') = 05;
+
+--6. hiredate 입사일이 1985-01-01 이후에 입사한사람 출력(컬럼: ename,hiredate,sal)
+select ename,hiredate,sal from emp where hiredate>'1985-01-01';
+
+--7 sal이 1500-3000 사이인 사람을 출력하는데 관계연산자 and 를 이용해서 출력
+select ename,sal from emp where sal>=1500 and sal <=3000;
+
+--8 7번의 결과를 between 을 이용해서 출력( ename,sal,mgr)
+select ename,sal from emp where sal between 1500 and 3000;
+
+--9 in 을 이용해 job이 clerk,president,manager인 사람의 전체 컬럼을 모두 출력
+select * from emp where job in('CLERK','PRESIDENT','MANAGER') order by job;
+
+--10 ename, sal, comm,sal*comm 을 출려ㅛㄱ하는데 comm이 null 인경우 1로 변경해서 출력 하고 계산
+select ename,sal,NVL(comm,1),NVL(sal+comm,sal) from emp;
+
+--11 ename 이 ADAMS인 사람의 SAL 보다 더 많이 받는 사람 출력 (ename,job,sal)
+select ename,job,sal from emp where sal>(select sal from emp where ename = 'ADAMS') order by ename;
+
+--12 평균 SAL보다 작은 사람의 젙체 컬럼 출력
+select * from emp where sal<(select round(avg(sal)) from emp);
+
+--13 ename 이 a나 s나 m으로 시작하는 모든 사람 출력(컬럼: ENAME,job,sal)
+select ename,job,sal from emp where  ename like 'A%' or ename like 'S%' or ename like 'M%';
+
+--14 MGR을 GROUP로 인원수와 평균 SAL을 구하시오(평균SAL은 무조건 올림으로(무조건 올림은 ELIM인가 그거임 )) 무조건 올림 뭐더라 ; 
+select count(*) 인원수, round(avg(sal),0) 평균 from emp group by mgr; 
+
+--15 SCOTT의 sal 과 같은 sal 을 받는 사람을 조회(ename,sal)
+select ename,sal from emp where sal = (select sal from emp where ename like 'SCOTT');
+
+--16 ENAME의 글자길이가 4글자인 사람만 조회(ename,job)
+select ename,job from emp where length(ename)=4;
+
+--17 ename의 3번째 글자가 R이거나 A인 사람 조회
+select ename,job from emp where ename like '__R%' or ename like '__A%';
+
+--18 Job 직업별로 인원수와 최고연봉을 출력하는데 직업의 오름차순으로 출력
+select job 직업, count(*) 인원수, max(sal) 최고연봉 from emp group by job order by job ;
+
+--19 ||연산자를 이용하여 다음과 같이 하나의 컬럼으로 출력하시오
+-- 예) Scott의 직업은 CLERK이며 입사년도는 1989년 05월 입니다.
+
+select ename||'의 직업은 '||job||'이며 입사년도는 '||to_char(hiredate,'yyyy')||'년 '||to_char(hiredate,'mm')||'월 입니다.' 자기소개 from emp;
+
+
--- DBwork/scott_1230.sql
+++ DBwork/scott_1230.sql
@@ -85,3 +85,120 @@ select * from emp where mgr is not null;
  -- Null과 숫자를 더하면 NULL 이 출력 
  --위의 경우 sal+comm이 Null 인 경우 0으로 출력
  select sal,comm,NVL(sal+comm,0) from emp; 
+ 
+ --컬럼 제목에 별칭(alias)
+ select ename as "사원명",sal as "월급여" from emp;
+ select ename "사원명", sal "월급여" from emp; -- as 생략하고 alias 지정하는 방법
+ select ename 사원명, sal 월급여 from emp; -- alias 에 공백이 없는 경우 "" 도 생략가능
+ select ename "사원 이름", sal 월급여 from emp; --이처럼 (사원 이름)제목에 공백이 들어가면 "" 생략 불가능 -> 공백이 있는경우 "" 를 반드시 써주기
+ 
+ --총 레코드 개수 (총 데이터 개수)
+ select count(*) from emp;
+ select count(*) count from emp; -- 이경우 컬럼명이 count 
+ select count(*) 총갯수 from emp;
+ 
+ select sal,comm,sal+NVL(comm,0) sum from emp;
+ select sal 월급여,NVL(comm,0) 커미션,sal+NVL(comm,0) 총금액 from emp;
+ --문자열을 컬럼에 추가시 || 연산자 사용.
+ select '내이름은 ' ||ename||' 입니다' 자기소개 from emp;
+ select '내 직업은 ' || job ||' 이고 내 월급은 '|| sal || '$입니다.  요로시구 오네가이시마스' 자기소개 from emp;
+ 
+select * from emp where sal between 1500 and 2000;
+select * from emp where sal not between 1500 and 2000;
+
+--not in 을 사용해 job이 sales man , clerk 이 아닌 것만 출력 ? 
+select * from emp where job not in('SALESMAN','CLERK');
+
+--group 함수(count sum avg max min) 등이 있음
+select count(*) from emp;
+select sum(sal) from emp;
+select round(avg(sal),2) from emp; -- avg 는 보통 소숫점이 엄청 길기 때문에 round 함수를 함께 사용해 (ex) 2번째 자리 까지 구한다.
+select max(sal) from emp;
+select min(sal) from emp;
+
+-- 평균 급여 보다sal 이 더 높은 사람을 조회하시오 
+select ename from emp where sal> (select round(avg(sal))from emp);
+
+--scott 과 같은 직업을 가진 사람을 구하시오
+select * from emp where  job = (select job from emp where ename = 'SCOTT');
+select * from emp where  mgr = (select mgr from emp where ename='SCOTT');
+
+-- group by
+select job from emp group by job;
+
+-- job 의 그룹별로 인원수를 구해보자 
+select job,count(*) 인원 from emp group by job;
+
+--제목에 별칭을 부여
+select job 직업,count(*) 인원 from emp group by job;
+select job 직업,count(*) 인원 from emp group by job having count(job)>=3;
+select job 직업,count(*) 인원수 from emp group by job having count(job)>=3 order by 2; -- or 인원수 
+
+--직업별 인원수 최저연봉, 최고연봉, 평균 연봉 (소수점 이하 없이)을 구하시오 
+select job 직업, count(*) 인원수,max(sal) 최고연봉, min(sal) 최저연봉, round(avg(sal)) 평균연봉 from emp group by job order by job;
+
+
+--- 48 page number function 
+select abs(-5), abs(5) from dual; -- 절대값
+
+select round(3.6,0),CEIL(3.6), Floor(3.6) from dual;--round 반올림 ceil 무조건 올림, floor 무조건 내림
+
+select round(328947,-2) from dual ; -- 328900 반올림 하는거임 해당 자리 까지 
+
+select round(avg(sal),0), ceil(avg(sal)),floor(avg(sal)) from emp;
+
+--power(M,n): m의n승값을 구합
+--mod(m,n) : m을n으로 나눈 나머지 값
+select power(3,3), mod(10,3) from dual; -- 27,1 3의 3제곱 and 10/3의 나머지 1
+
+--문자 함수 CONCAT(문자연결),LOWER(소문자 변환),UPPER(대문자 변환),INITCAP(첫글자만 대문자로)
+Select ename, concat(ename,'님'),lower(ename),upper(ename), initcap(ename) from emp;
+
+--lpad 왼쪽부터 지정한 문자로 변경하는것 
+select LPAD(sal,10,'*') from emp;
+--rpad lpad 반대 
+select rpad(sal,10,'*') from emp;
+
+select substr('Happy day',7,3) from dual; -- 7번 글자부터 3글자 추출 
+select substr('Happy day',-7,3) from dual; -- 뒤에서 부터 7번째 글자부터 3글자 추출 p p y
+
+select RPAD(substr(ename,1,3),7,'*') from emp;
+
+--length 길이 구하기, 
+select ename 이름, length(ename) 이름길이 from emp;
+
+--replace happy를 good 으로 변경해서 출력 
+select replace('Happy day','Happy','Good') from dual;
+
+--trim 앞 뒤 공백 제거
+select '*'||' Lee Su GEE  '||'*' from dual;
+select trim('    Lee Su GEE    ') from dual;--앞 뒤 공백 제거됨 
+
+--현재 날짜 를 나타내는 sysdate 
+select sysdate from dual;
+select sysdate+7 from dual;
+select sysdate+1 "내일 날짜" from dual;
+
+--to char 함수를 이용해 원하는 날짜 모양으로 출력 
+select to_char(sysdate,'yyyy-mm-dd') from dual;
+--mm:월 mi:분 hh:12시간 표시 am또는 pm: 오잔이나 오후 표시
+select to_char(sysdate,'yyyy-mm-dd am hh:mi') from dual;
+select to_char(sysdate,'yyyy-mm-dd hh24:mi') from dual;
+
+select to_char(sysdate,'month') from dual ;--지역에 따라 그 해당 언어로 나오기 때문에 에러가 발생할 수 있어 잘 사용하지 않는다.
+
+--년
+select to_char(sysdate, 'yyyy') from dual ;
+--월
+select to_char(sysdate, 'mm') from dual ;
+select to_char(to_date('2024-5-10'),'mm') from dual;
+--일
+select to_char(sysdate, 'dd') from dual;
+-- 요일
+select to_char(sysdate, 'dy') from dual;
+
+--emp의 hiredate 는 날짜타입
+select to_char(hiredate,'yyyy-mm-dd') 입사일 from emp;
+
+
+
